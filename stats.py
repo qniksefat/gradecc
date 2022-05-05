e52fc55aedea672d42dc74872c1a1d964bcce403
Qasem <qniksefat@gmail.com>
2022-05-05 16:52:21 -0400

    refactor rm_anova, merged with fdr correction

--- stats.py
+++ stats.py
@@ -1,39 +1,39 @@
 import pandas as pd
 import pingouin as pg
 from tqdm import tqdm
+
 tqdm.pandas()
 
 ALPHA = 0.05
 FDR_method = 'fdr_bh'
 
 
-def get_rm_anova(df: pd.DataFrame):
-    # todo merge fdr with it. by default have the fdr pvalue
+def rm_anova(df: pd.DataFrame):
     """computes repeated measures ANOVA for each region in epics
-
-    Args:
-        df:
-
     Returns:
         pd.DataFrame with F-statistic and p-values
     """
-    if df.epic.nunique() < 2:
+    if _has_one_epic(df):
         raise ValueError('Needs more than one epic to compute ANOVA within them')
     print('Computing repeated measures ANOVA...')
-    _df = df.groupby(['region', 'measure']).progress_apply(pg.rm_anova, dv='value', within='epic', subject='subject')
-    _df = _df.rename(columns={'p-unc': 'pvalue'})
-    _df = _df.reset_index()[['measure', 'region', 'F', 'pvalue']]
-    return _df
+    df = df.groupby(['region', 'measure']).progress_apply(pg.rm_anova, dv='value', within='epic', subject='subject')
+    df = df.rename(columns={'p-unc': 'pvalue'})
+    df = df.reset_index()[['measure', 'region', 'F', 'pvalue']]
+    df = _fdr_correction(df)
+    return df
 
 
-def append_fdr(df):
+def _has_one_epic(df):
+    return df.epic.nunique() < 2
+
+
+def _fdr_correction(df):
     df_list = []
     for measure in df.measure.unique():
         _df = df[df.measure == measure]
-        reject, pvalue_corrected = pg.multicomp(_df.pvalue.tolist(), alpha=ALPHA, method=FDR_method)
+        _df['fdr_significant'], _df['pvalue_corrected'] = \
+            pg.multicomp(_df.pvalue.tolist(), alpha=ALPHA, method=FDR_method)
         _df = _df.set_index('region')
-        _df['fdr_significant'] = reject
-        _df['pvalue_corrected'] = pvalue_corrected
         _df = _df.reset_index()
         df_list.append(_df)
     df = pd.concat(df_list, axis=0)
@@ -43,13 +43,16 @@ def append_fdr(df):
 def pairwise_ttests(df):
     # todo in input, if it's pd.df you should just provide column names
     """ FDR-corrected
-
-    Args:
-        df:
-
-    Returns:
-
     """
+    df_grouped, index = _seed_ttests(df)
+    df_stats_pairwise = df_grouped.progress_apply(pg.pairwise_ttests, dv='value', between='epic',
+                                                  subject='subject', padjust=FDR_method)
+    df_stats_pairwise = df_stats_pairwise.reset_index().set_index(index)[['region', 'T', 'p-corr']]
+    df_stats_pairwise = df_stats_pairwise.rename(columns={'p-corr': 'pvalue_corrected', 'T': 'tstat'})
+    return df_stats_pairwise
+
+
+def _seed_ttests(df):
     index = ['A', 'B']
     if 'measure' in df.columns:
         df_grouped = df.groupby(['measure', 'region'])
@@ -57,9 +60,4 @@ def pairwise_ttests(df):
     else:
         # for seed connectivity t-tests
         df_grouped = df.groupby(['region'])
-    df_stats_pairwise = df_grouped.progress_apply(pg.pairwise_ttests, dv='value', between='epic',
-                                                                subject='subject', padjust=FDR_method)
-
-    df_stats_pairwise = df_stats_pairwise.reset_index().set_index(index)[['region', 'T', 'p-corr']]
-    df_stats_pairwise = df_stats_pairwise.rename(columns={'p-corr': 'pvalue_corrected', 'T': 'tstat'})
-    return df_stats_pairwise
+    return df_grouped, index

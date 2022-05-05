a732b226d458dd7a17e5eb83761de62484c8f511
Qasem <qniksefat@gmail.com>
2022-05-05 16:25:46 -0400

    plot matrix threshold to visualize, remove old data files

--- connectivity_matrix.py
+++ connectivity_matrix.py
@@ -5,67 +5,55 @@ from nilearn.connectome import ConnectivityMeasure
 from load_timeseries import SUBJECTS, load_timeseries
 
 
-def plot_conn_mat(epic: str, subject=None):
-    connectivity_matrix, rois = get_conn_mat(epic, subject)
+def plot_conn_mat(epic: str, subject=None, significant_regions=True):
+    connectivity_matrix, regions = get_conn_mat(epic, subject)
 
-    ### todo Reduce conn matrix size, only for visualization purposes
+    if significant_regions:
+        connectivity_matrix, regions = _mask_conn_mat(connectivity_matrix, regions)
 
-    # mat_mask = np.where(np.std(correlation_matrix, axis=1) > 0.2)[0]
-    # c = correlation_matrix[mat_mask][:, mat_mask]
-    # # Create corresponding region names
-    # regions_list = ['%s_%s' % (h, r.decode()) for h in ['L', 'R'] for r in regions]
-    # masked_regions = [regions_list[i] for i in mat_mask]
+    plotting.plot_matrix(connectivity_matrix, labels=regions, reorder=True,
+                         colorbar=True, figure=(10, 15), vmax=0.8, vmin=-0.8)
 
-    plotting.plot_matrix(connectivity_matrix, labels=rois,
-                         colorbar=True, figure=(10, 15), vmax=0.8, vmin=-0.8, reorder=True)
+
+def _mask_conn_mat(conn_mat, regions):
+    sd_threshold = _variance_threshold(conn_mat)
+    conn_mat_mask = np.where(np.std(conn_mat, axis=1) > sd_threshold)[0]
+    conn_mat_masked = conn_mat[conn_mat_mask][:, conn_mat_mask]
+    regions_masked = [regions[i] for i in conn_mat_mask]
+    return conn_mat_masked, regions_masked
+
+
+def _variance_threshold(conn_mat):
+    sd_threshold = np.std(conn_mat, axis=1).mean()
+    return sd_threshold
 
 
 def get_conn_mat(epic: str, subject: int = None):
     """get connectivity matrix. If subject is None, matrix averaged over all subjects.
-
-    Args:
-        epic (str): _description_
-        subject (int, optional): when specified, get the resutl for one subject. Defaults to None.
-
-    Returns:
-        _type_: matrix in numpy array, list of ROIs
     """
     if subject is not None:
-        connectivity_matrix, rois = _get_conn_mat_subject(epic, subject)
+        connectivity_matrix, regions = _get_conn_mat_subject(epic, subject)
     else:
-        connectivity_matrix, rois = _get_conn_mat_averaged(epic)
-    return connectivity_matrix, rois
+        connectivity_matrix, regions = _get_conn_mat_averaged(epic)
+    return connectivity_matrix, regions
 
 
 def _get_conn_mat_subject(epic: str, subject: int):
-    """connectivity matrix within an epic for a subject 
-
-    Args:
-        epic (str): time period such as `rest`, `baseline`, etc
-        subject (int): subject identification number
-
-    Returns:
-        (np.array, list): connectivity matrix and regions of interest (ROIs)
+    """connectivity matrix within an epic for a subject
     """
     timeseries = load_timeseries(epic=epic, subject=subject)
-    rois = timeseries.columns.tolist()
+    regions = timeseries.columns.tolist()
     timeseries = timeseries.to_numpy()
     correlation_measure = ConnectivityMeasure(kind='correlation')
     # todo why only pearson correlation?
     connectivity_matrix = correlation_measure.fit_transform([timeseries])[0]
     np.fill_diagonal(connectivity_matrix, 0)
 
-    return connectivity_matrix, rois
+    return connectivity_matrix, regions
 
 
 def _get_conn_mat_averaged(epic: str):
     """connectivity matrix for an epic averaged over all subjects
-
-    Args:
-        epic (str): time period such as `rest`, `baseline`, etc
-
-    Returns:
-        (np.array, list): connectivity matrix and regions of interest (ROIs)
     """
     avg_matrix, rois = _get_conn_mat_subject(epic=epic, subject=SUBJECTS[0])
     avg_matrix = np.zeros(avg_matrix.shape)

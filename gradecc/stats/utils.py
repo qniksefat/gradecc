b5f7b6d0a6b28a11db707dff5d858dd973b21dbb
Qasem <qniksefat@gmail.com>
2022-06-10 11:28:58 -0400

    filenames for stats and made cache dir

--- gradecc/stats/utils.py
+++ gradecc/stats/utils.py
@@ -1,15 +1,6 @@
-from os import path
 from tqdm import tqdm
-
-from gradecc.utils.filenames import data_inside
 
 tqdm.pandas()
 
 ALPHA = 0.05
 FDR_method = 'fdr_bh'
-
-
-def _make_filename(file):
-    filename = file + '.csv'
-    filename = path.join(data_inside, filename)
-    return filename

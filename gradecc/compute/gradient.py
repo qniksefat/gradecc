03380ec0a5c05d332c75bd39a045c3a1d51d3b39
M Qasem Nik <qniksefat@gmail.com>
2022-06-26 18:05:11 -0400

    separate variance exp from gradient file

--- gradecc/compute/gradient.py
+++ gradecc/compute/gradient.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from brainspace.gradient import GradientMaps
 from tqdm import tqdm
 
@@ -11,103 +10,63 @@ SPARSITY = 0.9
 # can be a class variable, also not a semantically global var. what is it?
 
 
-def make_gradients(epic_list=None, subjects=SUBJECTS,
-                   num_components=NUM_COMPONENTS, reference_epic='baseline',
+def make_gradients(epoch_list=None, subjects=SUBJECTS,
+                   num_components=NUM_COMPONENTS, reference_epoch='baseline',
                    ) -> pd.DataFrame:
-    if epic_list is None:
-        epic_list = ['baseline', 'early', 'late']
-    gradient_reference = _make_reference_gradient(reference_epic)
+    if epoch_list is None:
+        epoch_list = ['baseline', 'early', 'late']
+    gradient_reference = _make_reference_gradient(reference_epoch)
     df = pd.DataFrame()
     print('Making gradients for subjects...')
     for subject in tqdm(subjects):
-        subject_gradient_model = _make_subject_gradients(subject=subject, epic_list=epic_list,
+        subject_gradient_model = _make_subject_gradients(subject=subject, epoch_list=epoch_list,
                                                          gradient_reference=gradient_reference,
                                                          dim_reduction_approach='pca')
-        for epic in epic_list:
+        for epoch in epoch_list:
             for component in range(num_components):
                 df_part = _make_subject_values(subject, subject_gradient_model,
-                                               component, epic, epic_list)
+                                               component, epoch, epoch_list)
                 df = pd.concat([df, df_part], axis=0)
     return df
 
 
-def _make_subject_values(subject, subject_gradient_model, component, epic, epic_list):
-    subject_gradients = _get_epic_component(subject_gradient_model,
-                                            component, epic, epic_list)
-    regions_epic_subject = get_conn_mat(epic, subject)[1]
-    df_part = _fill_df(subject_gradients, regions_epic_subject,
-                       subject, epic, component)
+def _make_subject_values(subject, subject_gradient_model, component, epoch, epoch_list):
+    subject_gradients = _get_epoch_component(subject_gradient_model,
+                                             component, epoch, epoch_list)
+    regions_epoch_subject = get_conn_mat(epoch, subject)[1]
+    df_part = _fill_df(subject_gradients, regions_epoch_subject,
+                       subject, epoch, component)
     return df_part
 
 
-def _make_subject_gradients(subject, epic_list, gradient_reference: GradientMaps,
+def _make_subject_gradients(subject, epoch_list, gradient_reference: GradientMaps,
                             dim_reduction_approach='pca'):
     """ if ref is None, takes the first as ref
     """
     gradient_model = GradientMaps(random_state=0, alignment="procrustes",
                                   approach=dim_reduction_approach)
-    conn_mat_epics = [get_conn_mat(epic, subject)[0] for epic in epic_list]
-    gradient_model.fit(conn_mat_epics, sparsity=SPARSITY,
+    conn_mat_epochs = [get_conn_mat(epoch, subject)[0] for epoch in epoch_list]
+    gradient_model.fit(conn_mat_epochs, sparsity=SPARSITY,
                        reference=gradient_reference.gradients_)
     return gradient_model
 
 
-def _get_epic_component(gradient_model: GradientMaps, component, epic, epic_list):
-    return gradient_model.aligned_[epic_list.index(epic)][:, component]
+def _get_epoch_component(gradient_model: GradientMaps, component, epoch, epoch_list):
+    return gradient_model.aligned_[epoch_list.index(epoch)][:, component]
 
 
 # todo is it enough to reference grad model? or we need normalize conn mats?
-def _make_reference_gradient(reference_epic, dim_reduction_approach='pca'):
-    global_conn_mat, _ = get_conn_mat(epic=reference_epic)
+def _make_reference_gradient(reference_epoch, dim_reduction_approach='pca'):
+    global_conn_mat, _ = get_conn_mat(epoch=reference_epoch)
     global_gradient_reference = GradientMaps(random_state=0, approach=dim_reduction_approach)
     global_gradient_reference.fit(global_conn_mat, sparsity=SPARSITY)
     return global_gradient_reference
 
 
-def _fill_df(values, regions, subject, epic, component):
+def _fill_df(values, regions, subject, epoch, component):
     df = pd.DataFrame(values, columns=['value'])
     df['region'] = regions
     df['subject'] = subject
-    df['epic'] = epic
+    df['epoch'] = epoch
     df['measure'] = 'gradient' + str(component + 1)
     return df
-
-
-# todo separate
-
-
-def variance_explained(epic_list=None, subjects=SUBJECTS,
-                       reference_epic='baseline', cum_sum=True
-                       ) -> pd.DataFrame:
-    """ the ratio of variance, explained by each principal component. for all epics.
-    """
-    if epic_list is None:
-        epic_list = ['baseline', 'early', 'late']
-    num_comp_default = 10
-    gradient_reference = _make_reference_gradient(reference_epic)
-    print('Computing variance explained...')
-    subjects_lambdas = []
-    for subject in tqdm(subjects):
-        subject_gradient_model = _make_subject_gradients(subject=subject, epic_list=epic_list,
-                                                         gradient_reference=gradient_reference,
-                                                         dim_reduction_approach='pca')
-        subjects_lambdas.append(np.stack(subject_gradient_model.lambdas_))
-    subjects_lambdas_avg = np.array(subjects_lambdas).mean(axis=0) / num_comp_default
-    # todo pass without averaging, plot with SD
-    variance = pd.DataFrame(subjects_lambdas_avg, index=epic_list,
-                            columns=list(np.arange(10)))
-    if cum_sum:
-        return _cum_sum(variance)
-    else:
-        return variance
-
-
-def _cum_sum(variance):
-    variance_cum_sum = pd.DataFrame()
-    for epic_idx in variance.index:
-        variance_cum_sum[epic_idx] = variance.loc[epic_idx].to_numpy().cumsum()
-    return variance_cum_sum.T
-
-
-if __name__ == '__main__':
-    pass
--- gradecc/compute/measures.py
+++ gradecc/compute/measures.py
@@ -11,30 +11,30 @@ tqdm.pandas()
 
 
 # todo decorator cache
-def get_measures(measures=None, epic_list=None,
+def get_measures(measures=None, epoch_list=None,
                  subjects=SUBJECTS) -> pd.DataFrame:
     """get measures for a brain region
     Args:
-        epic_list: list of epics such as `baseline`
+        epoch_list: list of epochs such as `baseline`
         measures (list): different measures such as gradients or eccentricity
         subjects (list): if not specified, default to all subjects
     Returns:
-        pd.DataFrame: measures corresponding to each region, subject, epic
+        pd.DataFrame: measures corresponding to each region, subject, epoch
     """
-    epic_list, measures, subjects = _init_inputs(epic_list, measures, subjects)
+    epoch_list, measures, subjects = _init_inputs(epoch_list, measures, subjects)
 
     if file_exists(measures_filename):
         print('Reading data from', measures_filename)
         df = pd.read_csv(measures_filename)
-        return df[(df.measure.isin(measures)) &
-                  (df.subject.isin(subjects)) &
-                  (df.epic.isin(epic_list))]
+        return df[(df['measure'].isin(measures)) &
+                  (df['subject'].isin(subjects)) &
+                  (df['epoch'].isin(epoch_list))]
     else:
         make_measures()
-        return get_measures(measures=measures, epic_list=epic_list, subjects=subjects)
+        return get_measures(measures=measures, epoch_list=epoch_list, subjects=subjects)
 
 
-def _init_inputs(epic_list, measures, subjects):
+def _init_inputs(epoch_list, measures, subjects):
     if measures is None:
         measures = _make_measures_list()
     elif not isinstance(measures, list):
@@ -45,12 +45,12 @@ def _init_inputs(epic_list, measures, subjects):
     elif not isinstance(subjects, list):
         subjects = [subjects]
 
-    if epic_list is None:
-        epic_list = ['baseline', 'early', 'late']
-    elif not isinstance(epic_list, list):
-        epic_list = [epic_list]
+    if epoch_list is None:
+        epoch_list = ['baseline', 'early', 'late']
+    elif not isinstance(epoch_list, list):
+        epoch_list = [epoch_list]
 
-    return epic_list, measures, subjects
+    return epoch_list, measures, subjects
 
 
 def _make_measures_list():
@@ -58,10 +58,10 @@ def _make_measures_list():
 
 
 # todo cache make_measures
-def make_measures(epic_list=None, subjects=SUBJECTS):
-    if epic_list is None:
-        epic_list = ['baseline', 'early', 'late']
-    df_gradients = make_gradients(epic_list=epic_list, subjects=subjects)
+def make_measures(epoch_list=None, subjects=SUBJECTS):
+    if epoch_list is None:
+        epoch_list = ['baseline', 'early', 'late']
+    df_gradients = make_gradients(epoch_list=epoch_list, subjects=subjects)
     df_ecc = _make_eccentricity(df_gradients)
     df_measures = pd.concat([df_gradients, df_ecc], axis=0)
     df_measures.to_csv(measures_filename, index=False)
@@ -79,18 +79,17 @@ def _make_eccentricity(df):
     print('Making eccentricity...')
     eccentricity = df \
         .assign(v=lambda r: r['value'] ** 2, aixs=1) \
-        .groupby(['subject', 'epic', 'region']).agg({'v': sum}) \
+        .groupby(['subject', 'epoch', 'region']).agg({'v': sum}) \
         .v.progress_apply(np.sqrt).reset_index() \
         .rename(columns={'v': 'value'})
     eccentricity['measure'] = 'eccentricity'
     return eccentricity
 
 
-def get_measures_avg(epic_list=None, measures=None,
+def get_measures_avg(epoch_list=None, measures=None,
                      subjects=SUBJECTS) -> pd.DataFrame:
-    # todo handle non valid inputs like laate. NOT important; remove these all at once
-    values = get_measures(measures, epic_list, subjects)
-    values = values.groupby(['region', 'epic', 'measure']) \
+    values = get_measures(measures, epoch_list, subjects)
+    values = values.groupby(['region', 'epoch', 'measure']) \
         .mean().drop('subject', axis=1) \
         .reset_index()
     return values

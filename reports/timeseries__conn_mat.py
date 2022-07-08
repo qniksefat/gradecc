6668690b84b14fcd457fe0534ba832edbec353aa
M Qasem Nik <qniksefat@gmail.com>
2022-07-07 23:23:44 -0400

    edited report timeseries

--- reports/timeseries__conn_mat.py
+++ reports/timeseries__conn_mat.py
@@ -1,9 +1,12 @@
-from gradecc.load_timeseries import load_ts
-from gradecc.connectivity_matrix import plot_conn_mat
+from gradecc.load_data import Timeseries
+from gradecc.plot.conn_mat import plot_conn_mat
+from gradecc.compute.conn_mat import get_conn_mat, compute_conn_mat
+
 
 if __name__ == '__main__':
-    print('timeseries data',
-          load_ts(subject=1, epoch='rest', include_subcortex=True))
+    ts = Timeseries(subject_id='AB1', epoch='rest', include_subcortex=False)
+    print('timeseries data', ts.data)
 
     plot_conn_mat(epoch='late', include_subcortex=True, reorder=True, significant_regions=True, output_file='late epoch')
 
+    print(compute_connx_mat(timeseries=ts)[0].shape)

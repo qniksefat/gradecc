c80c36785f1d2de115e93d30a17a59aeca3de558
M Qasem Nik <qniksefat@gmail.com>
2022-07-07 23:25:33 -0400

    moved to utils

--- gradecc/utils/__init__.py
+++ gradecc/utils/__init__.py
@@ -1,3 +1,16 @@
+import pandas as pd
+
+from gradecc.load_data import all_region_names
 
 
+def melt_df(df):
+    df.columns = all_region_names()
+    df = pd.melt(df.reset_index(),
+                 id_vars=['seed_region', 'epoch', 'subject'],
+                 value_vars=list(df.columns),
+                 var_name='region', value_name='value')
+    return df
 
+
+def file_exists(filename):
+    return os.path.isfile(filename)

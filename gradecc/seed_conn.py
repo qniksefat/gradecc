b52ecbed8135014722677eac1b610b13f8a23586
M Qasem Nik <qniksefat@gmail.com>
2022-07-26 10:07:24 -0400

    seed conn works with timeseries object

--- gradecc/seed_conn.py
+++ gradecc/seed_conn.py
@@ -2,16 +2,16 @@ import pandas as pd
 import numpy as np
 from scipy.stats import pearsonr
 
-from gradecc.load_data import load_ts
+from gradecc.load_data import Timeseries
 from gradecc.load_data.subject import SUBJECTS_INT
 from gradecc.load_data import all_region_names
 
 
-def seed_connectivity(seed_regions, epoch_list=None):
+def seed_connectivity(seed_regions, epoch_list=None, subjects=SUBJECTS_INT):
     epoch_list, seed_regions = _init_inputs_seed_conn(epoch_list, seed_regions)
     df_seed = pd.DataFrame()
     for epoch in epoch_list:
-        for subject in SUBJECTS_INT:
+        for subject in subjects:
             for seed_region in seed_regions:
                 _df = _compute_seed_conn(seed_region, subject, epoch)
                 df_seed = pd.concat([df_seed, _df], axis=0)
@@ -27,17 +27,19 @@ def _init_inputs_seed_conn(epoch_list, seed_regions):
     return epoch_list, seed_regions
 
 
-def _compute_seed_conn(seed_region, subject, epoch):
-    timeseries_subject = load_ts(subject, epoch)
-    timeseries_subject = timeseries_subject.transpose()
-    seed_timeseries = timeseries_subject.loc[seed_region]
-    timeseries_subject = timeseries_subject.to_numpy()
-    stat_map = np.zeros(timeseries_subject.shape[0])
-    for i in range(timeseries_subject.shape[0]):
-        stat_map[i] = pearsonr(seed_timeseries, timeseries_subject[i])[0]
-    _remask_previously_masked(stat_map, timeseries_subject)
+def _compute_seed_conn(seed_region, subject_id, epoch):
+    timeseries = Timeseries(subject_id=subject_id, epoch=epoch)
+    timeseries.load()
+    timeseries_data = timeseries.data
+    timeseries_data = timeseries_data.transpose()
+    seed_timeseries = timeseries_data.loc[seed_region]
+    timeseries_data = timeseries_data.to_numpy()
+    stat_map = np.zeros(timeseries_data.shape[0])
+    for i in range(timeseries_data.shape[0]):
+        stat_map[i] = pearsonr(seed_timeseries, timeseries_data[i])[0]
+    _remask_previously_masked(stat_map, timeseries_data)
     _df = pd.DataFrame([stat_map], columns=all_region_names())
-    _df['subject'] = subject
+    _df['subject'] = subject_id
     _df['epoch'] = epoch
     _df['seed_region'] = seed_region
     return _df
